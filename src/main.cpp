1047e59b7d10937fa1d87cd9b8be42913e3f5845
João Alex - Laptop <97920941+joaoalexarruda@users.noreply.github.com>
2024-01-23 19:10:56 -0300

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,165 +1,185 @@
-// Inclui bibliotecas necessárias
-#include <Arduino.h> // Este é o arquivo principal incluído do SDK do Arduino. Ele define todas as funções e tipos base usados na programação do Arduino.
-#include <WiFi.h> // Esta é uma biblioteca que permite ao Arduino se conectar a uma rede WiFi. Ela fornece funções para conectar a uma rede, desconectar, verificar o status da conexão, e mais.
-#include <PubSubClient.h> // Esta é uma biblioteca cliente para MQTT (Message Queuing Telemetry Transport), um protocolo de mensagens leve para pequenos sensores e dispositivos móveis. É frequentemente usado em aplicações de IoT.
-#include <DHT.h> // Esta é uma biblioteca para a série DHT de sensores de temperatura/umidade de baixo custo. Ela fornece funções para ler a temperatura e a umidade desses sensores.
-#include <Adafruit_Sensor.h> // Esta é uma classe base para sensores da empresa Adafruit. É usada em conjunto com bibliotecas de sensores específicos (como a biblioteca DHT mencionada acima) para ler dados de sensores Adafruit.
+/*
+ * File: main.cpp
+ * Author: João Alex Arruda da Silva
+ * Date: 2024-Jan
+ * Description: This file contains the code for the ESP32 microcontroller
+ *              that reads the temperature and humidity from the DHT11 sensor
+ *              then calculates the moving average of the last 5 readings
+ *              and publishes the values to a MQTT broker.
+ *
+ */
 
-// Wi-Fi login e senha
+// Include the necessary libraries
+#include <Adafruit_Sensor.h>
+#include <Arduino.h>
+#include <DHT.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+// SSID and password for the Wi-Fi network
 const char *ssid = "joaoalex1";
 const char *password = "joao1579";
 
-// IPv4 e porta
-#define mqtt_server "192.168.29.165"
-#define mqtt_port 1883
+// MQTT broker address and port
+const char *mqtt_server = "192.168.29.165";
+const int mqtt_port = 1883;
 
-// Pino utilizado no ESP32 e sensor DHT11
+// Number of readings to calculate the moving average
+const int numReadings = 5;
+int readingsIndex = 0;
+
+/*
+ * These two lines of code are declaring and initializing arrays of floats
+ * with all elements set to -1.0. It is a placeholder value to indicate that
+ * the array element has not been set yet. Later, when we read the temperature
+ * and humidity from the DHT11 sensor, we will update the array with the new
+ * values.
+ */
+float temperatureReadings[numReadings] = {-1.0};
+float humidityReadings[numReadings] = {-1.0};
+
+// Defines the DHT pin and type
 #define DHTPIN 4
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-// Inicializar cliente Wi-Fi e MQTT
+/* This line creates an instance of the WiFiClient class which represents a TCP
+   client that can connect to a specified internet IP address and port. */
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Buffer circular para leituras
-const int numReadings = 7;
-int readingsIndex = 0;
-
+// Function to setup the Wi-Fi connection
 void setup_wifi() {
-  // Conectar ao Wi-Fi
-  delay(10);
-  Serial.println("Conectando ao Wi-Fi");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conectando...");
-  }
-  Serial.println("Conectado ao Wi-Fi");
+    delay(10);
+    Serial.println("Connecting to " + String(ssid) + "...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting...");
+    }
+    Serial.println("Connected to " + String(ssid) + " network!");
 }
 
+// Function to reconnect to the MQTT broker
 void reconnect() {
-  // Reconectar ao broker MQTT
-  while (!client.connected()) {
-    Serial.print("Tentando conexão MQTT...");
-    if (client.connect("ESP32Client")) {
-      Serial.println("Conectado ao broker MQTT");
-    } else {
-      Serial.print("Falhou, rc=");
-      Serial.print(client.state());
-      Serial.println(" Tentando novamente em 5 segundos...");
-      delay(5000);
+    while (!client.connected()) {
+        Serial.print("Trying to connect to MQTT broker...");
+        if (client.connect("ESP32Client")) {
+            Serial.println("Connected!");
+        } else {
+            Serial.print("Failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" Retrying in 5 seconds...");
+            delay(5000);
+        }
     }
-  }
 }
 
+// Function to update the readings array
 void updateReadings(float *readings, float newValue) {
-  // Atualizar o buffer circular de leituras
-  readings[readingsIndex] = newValue;
-  readingsIndex = (readingsIndex + 1) % numReadings;
+    readings[readingsIndex] = newValue;
+    readingsIndex = (readingsIndex + 1) % numReadings;
 }
 
-// Inicia arrays com -1
-float temperatureReadings[numReadings] = {-1.0};
-float humidityReadings[numReadings] = {-1.0};
-
+/*
+ * Function to calculate the moving average of the last 5 readings.
+ * It takes an array of floats as an argument and returns a float.
+ * The function iterates over the array and calculates the sum of all
+ * the values that are not equal to -1.0. Then it divides the sum by
+ * the number of values that are not equal to -1.0 and returns the result.
+ */
 float calculateMovingAverage(float *readings) {
-  float sum = 0;
-  int count = 0;
+    float sum = 0;
+    int count = 0;
 
-  // Calcular a soma dos valores válidos
-  for (int i = 0; i < numReadings; i++) {
-    if (readings[i] != -1.0) {
-      sum += readings[i];
-      count++;
+    for (int i = 0; i < numReadings; i++) {
+        if (readings[i] != -1.0) {
+            sum += readings[i];
+            count++;
+        }
     }
-  }
 
-  // Verificar se há pelo menos um valor válido
-  if (count > 0) {
-    return sum / count;
-  } else {
-    return -1.0;  // Retorna um valor indicando que não há dados válidos suficientes para calcular média.
-  }
+    if (count > 0) {
+        return sum / count;
+    } else {
+        return -1.0;
+    }
 }
 
-
-
+// Main function
 void setup() {
-  // Inicializar o ESP32 e o sensor DHT
-  Serial.begin(115200);
-  dht.begin();
-  // Inicializar o Wi-Fi e o MQTT
-  setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
+    Serial.begin(115200);  // Initialize serial communication
+    dht.begin();           // Initialize DHT sensor
+    setup_wifi();          // Setup Wi-Fi connection
+    client.setServer(mqtt_server, mqtt_port);  // Setup MQTT broker
 }
 
+// Loop function
 void loop() {
-  // Reconectar ao broker MQTT se a conexão cair
-  if (!client.connected()) {
-    reconnect();
-  }
+    // Check if the client is connected to the MQTT broker
+    if (!client.connected()) {
+        reconnect();
+    }
 
-  // Ler temperatura e umidade do sensor DHT
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
+    // Read temperature and humidity from the DHT11 sensor
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
 
-  if (isnan(humidity) || isnan(temperature)) {
-    // Ignorar leituras inválidas
-    Serial.println("Falha ao ler do sensor DHT");
-    delay(2000);
-    return;
-  }
+    // Check if any reading is invalid
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(2000);
+        return;
+    }
 
-  // Atualizar leituras
-  updateReadings(temperatureReadings, temperature);
-  updateReadings(humidityReadings, humidity);
+    // Update the readings array
+    updateReadings(temperatureReadings, temperature);
+    updateReadings(humidityReadings, humidity);
 
-  // Calcular médias móveis
-  float avgTemperature = calculateMovingAverage(temperatureReadings);
-  float avgHumidity = calculateMovingAverage(humidityReadings);
+    // Calculate the moving average of the last 5 readings
+    float avgTemperature = calculateMovingAverage(temperatureReadings);
+    float avgHumidity = calculateMovingAverage(humidityReadings);
 
-  // Converter valores float para strings para payload MQTT
-  char tempStr[8];
-  dtostrf(temperature, 2, 2, tempStr);
+    // Convert the float values to strings
+    char tempStr[8];
+    dtostrf(temperature, 2, 2, tempStr);
+    char humStr[8];
+    dtostrf(humidity, 2, 2, humStr);
+    char avgTempStr[8];
+    dtostrf(avgTemperature, 2, 2, avgTempStr);
+    char avgHumStr[8];
+    dtostrf(avgHumidity, 2, 2, avgHumStr);
 
-  char humStr[8];
-  dtostrf(humidity, 2, 2, humStr);
+    // Name of the topics to publish the values
+    char tempTopic[] = "esp32/temperature";
+    char humTopic[] = "esp32/humidity";
+    char avgTempTopic[] = "esp32/moving_average_temperature";
+    char avgHumTopic[] = "esp32/moving_average_humidity";
 
-  char avgTempStr[8];
-  dtostrf(avgTemperature, 2, 2, avgTempStr);
+    // Publish the values to the MQTT broker
+    client.publish(tempTopic, tempStr);
+    client.publish(humTopic, humStr);
+    client.publish(avgTempTopic, avgTempStr);
+    client.publish(avgHumTopic, avgHumStr);
 
-  char avgHumStr[8];
-  dtostrf(avgHumidity, 2, 2, avgHumStr);
+    // Print the values and topics to the serial monitor for debugging
+    Serial.println();
+    Serial.println("+-------------------+-------------------+");
+    Serial.println("| Parameter         | Value             |");
+    Serial.println("+-------------------+-------------------+");
+    Serial.printf("| Temperature       | %-17.2f |\n", temperature);
+    Serial.printf("| Humidity          | %-17.2f |\n", humidity);
+    Serial.printf("| Moving Avg Temp.  | %-17.2f |\n", avgTemperature);
+    Serial.printf("| Moving Avg Humid. | %-17.2f |\n", avgHumidity);
+    Serial.println("+-------------------+-------------------+");
+    Serial.println("|            Published Topic            |");
+    Serial.println("+-------------------+-------------------+");
+    Serial.println("| " + String(tempTopic) + "                     |");
+    Serial.println("| " + String(humTopic) + "                        |");
+    Serial.println("| " + String(avgTempTopic) + "      |");
+    Serial.println("| " + String(avgHumTopic) + "         |");
+    Serial.println("+-------------------+-------------------+");
 
-  // Tópicos MQTT para publicar os dados
-  char tempTopic[] = "esp32/temperatura";
-  char humTopic[] = "esp32/umidade";
-  char avgTempTopic[] = "esp32/media_movel_temperatura";
-  char avgHumTopic[] = "esp32/media_movel_umidade";
-
-  // Publicar valores atuais e médias móveis nos tópicos MQTT
-  client.publish(tempTopic, tempStr);
-  client.publish(humTopic, humStr);
-  client.publish(avgTempTopic, avgTempStr);
-  client.publish(avgHumTopic, avgHumStr);
-
-  // Imprimir informações de debug
-  Serial.println();
-  Serial.println("+-------------------+-------------------+");
-  Serial.println("| Parâmetro         | Valor             |");
-  Serial.println("+-------------------+-------------------+");
-  Serial.printf("| Temperatura       | %-17.2f |\n", temperature);
-  Serial.printf("| Umidade           | %-17.2f |\n", humidity);
-  Serial.printf("| Média Móvel Temp. | %-17.2f |\n", avgTemperature);
-  Serial.printf("| Média Móvel Umid. | %-17.2f |\n", avgHumidity);
-  Serial.println("+-------------------+-------------------+");
-  Serial.println();
-
-  Serial.println("Publicado temperatura no tópico MQTT: " + String(tempTopic));
-  Serial.println("Publicado umidade no tópico MQTT: " + String(humTopic));
-  Serial.println("Publicada média móvel de temperatura no tópico MQTT: " + String(avgTempTopic));
-  Serial.println("Publicada média móvel de umidade no tópico MQTT: " + String(avgHumTopic));
-
-  delay(5000);
+    // Wait 3 seconds before reading the sensor again
+    delay(3000);
 }
